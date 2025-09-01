80bd019668c02ea716ad296c28356d779743d9d4
Benjamin Rasoli (student syh) <benjamin.rasoli@yh.nackademin.se>
2025-09-01 17:22:07 +0200

    implement login form with email and password fields

--- src/app/(auth)/(pages)/login/page.tsx
+++ src/app/(auth)/(pages)/login/page.tsx
@@ -1,7 +1,47 @@
-import React from "react";
+"use client";
+import { useState } from "react";
 
 function Page() {
-  return <div>Loing åpage</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log("Email:", email);
+    console.log("Password:", password);
+    setEmail("");
+    setPassword("");
+  };
+
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="flex w-72 flex-col items-center gap-4 rounded bg-white p-6 shadow-2xl">
+        <h1 className="text-2xl font-bold text-gray-800">Login</h1>
+        <form className="flex w-full flex-col gap-3 " onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="email"
+            className="rounded border px-3 py-2"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="rounded border px-3 py-2"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button
+            type="submit"
+            className="rounded bg-blue-600 py-2 text-white hover:bg-blue-700"
+          >
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Page;
